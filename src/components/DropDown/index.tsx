6180f9aa7fcddd714369a13e0a90262f16e5cccc
jason aghedo <jason.aghedo@gmail.com>
2024-04-22 14:57:01 +0100

    fix: Vercel Deploymenat issues

---
+++ src/components/DropDown/index.tsx
@@ -0,0 +1,64 @@
+import { defaultEase } from "../../utils/framer-default-animations";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
+const DropDown = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [currentValue, setCurrentValue] = useState<null | string>();
+
+	const selectValue = (value: "Male" | "Female") => {
+		setCurrentValue(value);
+		setIsOpen(false);
+	};
+	return (
+		<div>
+			<button
+				onClick={() => setIsOpen(!isOpen)}
+				className="text-[#555] focus:outline-none text-xs text-center inline-flex items-center justify-between w-full py-[18px] px-4 border-b border-[#E8E8E8]"
+				type="button"
+			>
+				{currentValue || "Select your gender"}
+				<img
+					src="/assets/icons/chevron-down.svg"
+					alt=""
+				/>
+			</button>
+			<AnimatePresence>
+				{isOpen ? (
+					<motion.div
+						key="dropdown"
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 0.3, ease: defaultEase }}
+						className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-full mt-3"
+					>
+						<ul
+							className="my-2 text-sm text-gray-700"
+							aria-labelledby="dropdownDefaultButton"
+						>
+							<li>
+								<a
+									onClick={() => selectValue("Male")}
+									className="block cursor-pointer px-4 py-2 hover:bg-gray-100"
+								>
+									Male
+								</a>
+							</li>
+							<li>
+								<a
+									onClick={() => selectValue("Female")}
+									className="block cursor-pointer px-4 py-2 hover:bg-gray-100"
+								>
+									Female
+								</a>
+							</li>
+						</ul>
+					</motion.div>
+				) : null}
+			</AnimatePresence>
+		</div>
+	);
+};
+
+export default DropDown;
---
+++ src/components/PageContainer/index.tsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+import { pageTransition } from "../../utils/framer-default-animations";
+import React from "react";
+
+type PageContainerProps = {
+	className?: string;
+	children: JSX.Element | JSX.Element[];
+	style?: React.CSSProperties;
+};
+const PageContainer = ({
+	className,
+	children,
+	...props
+}: PageContainerProps) => {
+	return (
+		<motion.main
+			data-scroll-container
+			className={className}
+			{...pageTransition}
+			{...props}
+		>
+			{children}
+		</motion.main>
+	);
+};
+
+export default PageContainer;
